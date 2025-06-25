7dbce3911f2e2e9c7307c3fc234badd97b56833f
PitcherDev <cantaro755@gmail.com>
2025-06-25 11:27:47 +0200

    añadida pagina LoginScreen

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+import 'package:mindmate/core/app_colors.dart';
+import 'package:mindmate/core/constants.dart';
 
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key});
 
-class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        title: const Text('Bienvenid@ de nuevo'),
+        backgroundColor: AppColors.primary,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(AppSizes.padding),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Logo o imágen decorativa ********* TEMPORAL *********
+                  Image.asset("assets/images/home_image.png", height: 160),
+                  const SizedBox(height: 24),
+
+                  // Formulario email y contraseña
+                  const LoginForm(),
+                  const SizedBox(height: 24),
+
+                  // Separador visual
+                  const Text("o", style: TextStyle(fontSize: 16, color: Colors.black54)),
+                  const SizedBox(height: 16),
+
+                  // Botón de Google
+                  const GoogleLoadingButton(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
