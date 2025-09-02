6a4698d3b14bf9ba0adbd40a49a303d8cb4ca7c3
PitcherDev <cantaro755@gmail.com>
2025-09-02 20:26:23 +0200

    Agregada página que muestra los detalles de la técnica de relajación technique_full_screen, falta añadir función al boton de Comenzar/reproducir

---
+++ lib/features/home/screen/technique_full_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:mindmate/core/app_colors.dart';
+import 'package:mindmate/models/technique_model.dart';
+
+class TechniqueFullScreen extends StatelessWidget {
+  final Technique technique;
+
+const TechniqueFullScreen({ 
+  Key? key, 
+  required this.technique,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: Colors.black),
+          onPressed:() => Navigator.pop(context),
+        ),
+      ),
+      body: Hero(
+        tag: 'technique_${technique.id}',
+        child: Material(
+          color: Colors.transparent,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(technique.title,
+                style: Theme.of(context)
+                .textTheme
+                .headlineMedium
+                ?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      technique.description,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // Aquí el futuro reproducir sonido al iniciar ejercicio
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(48),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    icon: const Icon(Icons.play_arrow),
+                    label: const Text(">>Comenzar!"),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+  }
+}
